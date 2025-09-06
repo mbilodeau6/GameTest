64537d85c8917129693ab469907b4145e4bbcb16
Michael Bilodeau <mbilodeau6@hotmail.com>
2025-09-05 19:45:34 -0700

    Initial push

---
+++ GameBoard.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace GameTest.GameBoard;
+
+public class GameBoard
+{
+    private readonly ILogger<GameBoard> _logger;
+
+    public GameBoard(ILogger<GameBoard> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("GameBoard")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Some day... there will be an awesome online game site here. Well... the backend for an awesome online game site!");
+    }
+}
