6dfa77d4b29a28def3d086b73074d3e02ef9b26a
Michael Bilodeau <mbilodeau6@hotmail.com>
2025-11-14 20:17:05 -0700

    Added baseStates to GoalStats

--- GameTest.Tests/GoalStatsTests.cs
+++ GameTest.Tests/GoalStatsTests.cs
@@ -47,7 +47,7 @@ public class GoalStatsTests
     }
 
     [Fact]
-    public void Constructor_ValidParameters_PropertiesSet()
+    public void Constructor_NoBaseStats_PropertiesSet()
     {
         // Arrange
         var t1 = new Tile(ResourceType.Wood, 8, 0, 0);
@@ -68,7 +68,53 @@ public class GoalStatsTests
                 continue;
 
             Assert.True(goalStats.ResourceAcquisitionRates.ContainsKey(rt));
-            Assert.Equal(0.0, goalStats.ResourceAcquisitionRates[rt]);
+
+            if (rt == ResourceType.Wood || rt == ResourceType.Brick)
+                Assert.Equal(5.0 / 36.0, goalStats.ResourceAcquisitionRates[rt]);
+            else
+                Assert.Equal(0.0, goalStats.ResourceAcquisitionRates[rt]);
+        }
+
+        Assert.Equal(0, goalStats.RoadsNeeded);
+        Assert.Equal(0.0, goalStats.InterceptionRisk);
+        Assert.Equal(0.0, goalStats.OverallScore);
+    }
+    
+        [Fact]
+    public void Constructor_WithBaseStats_PropertiesSet()
+    {
+        // Arrange
+        var t1 = new Tile(ResourceType.Wood, 8, 0, 0);
+        var t2 = new Tile(ResourceType.Brick, 6, 2, 0);
+        var vertex = new Vertex(t1, t2, null);
+        int tradeRate = 3;
+
+        Dictionary<ResourceType, double> baseStats = new Dictionary<ResourceType, double>();
+        baseStats[ResourceType.Wood] = 4.0 / 36.0;
+        baseStats[ResourceType.Ore] = 1.0 / 36.0;
+
+        // Act
+        var goalStats = new GoalStats(vertex, tradeRate, baseStats);
+
+        // Assert
+        Assert.Equal(vertex.Id, goalStats.TargetVertex.Id);
+        Assert.Equal(tradeRate, goalStats.TradeRate);
+        Assert.NotNull(goalStats.ResourceAcquisitionRates);
+        foreach (ResourceType rt in Enum.GetValues(typeof(ResourceType)))
+        {
+            if (rt == ResourceType.Desert)
+                continue;
+
+            Assert.True(goalStats.ResourceAcquisitionRates.ContainsKey(rt));
+
+            if (rt == ResourceType.Wood)
+                Assert.Equal((5.0 + 4.0) / 36.0, goalStats.ResourceAcquisitionRates[rt]);
+            else if (rt == ResourceType.Brick)
+                Assert.Equal(5.0 / 36.0, goalStats.ResourceAcquisitionRates[rt]);
+            else if (rt == ResourceType.Ore)
+                Assert.Equal(1.0 / 36.0, goalStats.ResourceAcquisitionRates[rt]);
+            else
+                Assert.Equal(0.0, goalStats.ResourceAcquisitionRates[rt]);
         }
 
         Assert.Equal(0, goalStats.RoadsNeeded);
--- Models/GoalStats.cs
+++ Models/GoalStats.cs
@@ -1,5 +1,6 @@
 using Azure.Storage.Blobs.Models;
 using GameTest.DTOs;
+using GameTest.Services;
 
 namespace GameTest.Models;
 
@@ -7,12 +8,15 @@ public class GoalStats
 {
     public Vertex TargetVertex { get; }
     public Dictionary<ResourceType, double> ResourceAcquisitionRates { get; }
+    // TODO: Consider changing to or adding EstRoundsToAchieve and EstRoundsToIntercept.
+    // Something that identifies how likely achieving this goal is.
     public int RoadsNeeded { get; set; }
     public double InterceptionRisk { get; set; }
     public double OverallScore { get; set; }
     public int TradeRate { get; }
+   
 
-    public GoalStats(Vertex targetVertex, int tradeRate)
+    public GoalStats(Vertex targetVertex, int tradeRate, Dictionary<ResourceType, double>? baseStats = null)
     {
         if (targetVertex == null)
             throw new ArgumentNullException("targetVertex");
@@ -30,6 +34,19 @@ public class GoalStats
 
             ResourceAcquisitionRates[rt] = 0.0;
         }
+
+        foreach (var tile in targetVertex.Tiles)
+        {
+            if (tile.Resource == ResourceType.Desert)
+                continue;
+
+            ResourceAcquisitionRates[tile.Resource] += AIHelpers.GetProbabilityForDiceRoll(tile.DiceNumber);
+        }
+        
+        if (baseStats != null)
+            foreach(var rt in baseStats.Keys.ToList())
+                if (rt != ResourceType.Desert)
+                    ResourceAcquisitionRates[rt] += baseStats[rt];
         
         RoadsNeeded = 0;
         InterceptionRisk = 0.0;
