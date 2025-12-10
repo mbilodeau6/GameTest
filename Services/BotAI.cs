7ae3a8a7a3e540320351a7649af3b3940501de85
Michael Bilodeau <mbilodeau6@hotmail.com>
2025-12-09 19:31:45 -0700

    Fixed bug that was preventing bot from making more than one move per round.

--- Services/BotAI.cs
+++ Services/BotAI.cs
@@ -201,6 +201,8 @@ public class BotAI
             throw new InvalidOperationException("Current player must be identified and must be a Bot.");
 
         var move = DeterminePreferredMove();
+        if (move.VertexMove != null && move.EdgeMove != null && move.BuyDevelopmentCard)
+            return move;
 
         var tradeAnalysis = AnalyzePossibleBankTrades();
         if (tradeAnalysis.CanTrade && tradeAnalysis.TradeRequest != null)
